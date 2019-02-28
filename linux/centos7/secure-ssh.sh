03208ad71ade568d004c04feb4c20f6c4727c53e
jaredwhichard <jared.whichard@mymail.champlain.edu>
2019-02-27 19:41:17 -0500

    Script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -6,26 +6,35 @@
 #Adds a public key to that users authorized keys file
 #Removes roots ability to ssh in
 
-echo "testing 321"
-#Accept a variable to be used as the username
-read ‘Enter a username:’ user
+#Accept first  variable to be used as the username
+user=$1
 
 #Add user and make a .ssh file in the user's home directory
-useradd $user
-mkdir /home/$user/.ssh
+sudo useradd $user
+sudo mkdir /home/$user/.ssh
 
 #Copy the public key to the new named user
-cp linux/public-keys/sys265.pub /home/$user/.ssh/authorized_keys
+sudo cp ~/sys265-scripts/linux/public-keys/sys265.pub /home/$user/.ssh/authorized_keys
 
 #Allow user read write and execute
-chmod 700 /home/sys265/.ssh
+sudo chmod 700 /home/sys265/.ssh
 
 #Allow user read and write
-chmod 600 /home/$user/.ssh/authorized_keys
+sudo chmod 600 /home/$user/.ssh/authorized_keys
 
 #Assigning the user as the owner of the file and directory recursively
-chown -R $user:$user /home/$user/.ssh
+sudo chown -R $user:$user /home/$user/.ssh
+
+#Allow all to access sshd config 
+sudo chmod 777 /etc/ssh/sshd_config
 
 #Change the Permit root login to no globally
-sed 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-echo "Complete"
+sudo sed "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+
+#Revert to user access only
+sudo chmod 700 /etc/ssh/sshd_config
+
+#Resart sshd to save changes
+sudo systemctl restart sshd
+
+sudo echo "Complete"
